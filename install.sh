3f966c3b082efd81dba7493b2a453df27d1006d8
Rudy Hardeman <zarya@gigafreak.net>
2014-08-08 16:12:52 +0200

    Added install.h

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -rf libs
+rm -rf templibs
+mkdir templibs
+mkdir libs
+cd templibs
+echo "Downloading RF24"
+git clone https://github.com/TMRh20/RF24.git
+echo "Downloading RF24Network"
+git clone https://github.com/TMRh20/RF24Network.git
+cp -R RF24/RPi/RF24 ../libs 
+cp -R RF24Network/RPi/RF24Network ../libs 
+cd ..
+rm -rf templibs
+cd libs/RF24
+echo "Compiling RF24"
+make 
+make install
+cd ../RF24Network
+echo "Compiling RF24Network"
+make
+make install
+echo "Now build NrfPiNode"
